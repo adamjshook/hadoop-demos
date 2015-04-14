8db904d78d48181a2703fab632d03227af8f5132
Adam Shook <shadam1@shaina-mugans-macbook.local>
2015-04-14 18:04:15 -0400

    Commit avro demo code for python and java

---
+++ avro/AvroDemo/src/main/java/com/adamjshook/demo/avro/Driver.java
@@ -0,0 +1,119 @@
+package com.adamjshook.demo.avro;
+
+import java.io.File;
+
+import org.apache.avro.AvroRuntimeException;
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import example.avro.User;
+
+public class Driver {
+	public static void main(String[] args) throws Exception {
+		demoTypedObjects();
+		demoGenericObjects();
+	}
+
+	private static void demoTypedObjects() throws Exception {
+		System.out.println("-- Using Users.java objects --\n");
+
+		// Let's create some User objects
+		User user1 = new User();
+		user1.setName("Alyssa");
+		user1.setFavoriteNumber(256);
+		// Leave favorite color null
+
+		// Alternate constructor
+		User user2 = new User("Ben", 7, "red");
+
+		// Construct via builder
+		User user3 = User.newBuilder().setName("Charlie")
+				.setFavoriteColor("blue").setFavoriteNumber(null).build();
+
+		File file = new File("users.avro");
+		DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(
+				User.class);
+		DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(
+				userDatumWriter);
+
+		dataFileWriter.create(user1.getSchema(), new File("users.avro"));
+
+		dataFileWriter.append(user1);
+		dataFileWriter.append(user2);
+		dataFileWriter.append(user3);
+		dataFileWriter.close();
+
+		// Deserialize Users from disk
+		DatumReader<User> userDatumReader = new SpecificDatumReader<User>(
+				User.class);
+		DataFileReader<User> dataFileReader = new DataFileReader<User>(file,
+				userDatumReader);
+		User user = null;
+		while (dataFileReader.hasNext()) {
+			// Reuse user object by passing it to next(). This saves us from
+			// allocating and garbage collecting many objects for files with
+			// many items.
+			user = dataFileReader.next(user);
+			System.out.println(user);
+		}
+		dataFileReader.close();
+	}
+
+	private static void demoGenericObjects() throws Exception {
+		System.out.println("-- Using GenericRecords from user.avsc --\n");
+
+		Schema schema = new Schema.Parser().parse(new File("../user.avsc"));
+		GenericRecord user1 = new GenericData.Record(schema);
+		user1.put("name", "Alyssa");
+		user1.put("favorite_number", 256);
+		// Leave favorite color null
+
+		try {
+			user1.put("favorite_animal", "cat");
+		} catch (AvroRuntimeException e) {
+			System.err.println("Caught a AvroRuntimeException: "
+					+ e.getMessage());
+		}
+
+		GenericRecord user2 = new GenericData.Record(schema);
+		user2.put("name", "Ben");
+		user2.put("favorite_number", 7);
+		user2.put("favorite_color", "red");
+
+		// Serialize user1 and user2 to disk
+		File file = new File("users.avro");
+		DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<GenericRecord>(
+				schema);
+		DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<GenericRecord>(
+				datumWriter);
+		dataFileWriter.create(schema, file);
+		dataFileWriter.append(user1);
+		dataFileWriter.append(user2);
+		dataFileWriter.close();
+
+		// // Deserialize users from disk
+		DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>(
+				schema);
+		DataFileReader<GenericRecord> dataFileReader = new DataFileReader<GenericRecord>(
+				file, datumReader);
+		GenericRecord user = null;
+		while (dataFileReader.hasNext()) {
+			// Reuse user object by passing it to next(). This saves us from
+			// allocating and garbage collecting many objects for files with
+			// many items.
+			user = dataFileReader.next(user);
+			System.out.println(user);
+		}
+
+		dataFileReader.close();
+	}
+}
---
+++ avro/README.md
@@ -0,0 +1,46 @@
+
+
+# Avro Demo
+This is some code taken from [the Apache Avro website](http://avro.apache.org/docs/current/gettingstartedjava.html) for basic examples of working with Avro in Java and Python.
+
+
+----------
+
+Java
+-----
+
+Download the latest avro-tools jar file from the Apache Avro Download page: http://www.apache.org/dyn/closer.cgi/avro/
+
+`$ wget [jarfile]`
+
+Using the avro tools jar file, compile the user.avsc file
+
+`$ java -jar /path/to/avro-tools-1.7.7.jar compile schema user.avsc .`
+
+You now have a Java source file called `example/avro/Users.java` in your working directory.  Take a look at it now if you'd like.
+
+Move this folder to the `AvroDemo/src/main/java` folder so we can use it in our program.
+
+`$ mv example AvroDemo/src/main/java`
+
+Open up Eclipse and import the Maven project `AvroDemo/pom.xml`.
+
+From here, you can look at the code in `AvroDemo/src/main/java/com/adamjshook/demo/avro/Driver.java` for some simple usage of the Java object we generated as well as using GenericRecords from an Avro schema.
+
+Execute the code to see the output.
+
+Python
+--------
+
+If you have `pip`, use the below command to install Avro.  If you don't, then you can follow the instructions on Apache's website for downloading and installing the Avro module for Python.
+
+Or just download `pip` cause it's awesome.
+
+`$ sudo pip install avro`
+
+You can now view and execute `demo.py` to view the output.
+
+`$ python demo.py`
+
+Note that there is no support for code generation with Python.
+
