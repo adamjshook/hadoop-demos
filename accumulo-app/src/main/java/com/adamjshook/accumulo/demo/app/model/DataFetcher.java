29db011c48d20b1bb60b0fda8df8f6629b1e506d
Adam J Shook <adamjshook@gmail.com>
2016-03-27 12:16:59 -0400

    Comment DataFetcher

--- accumulo-app/src/main/java/com/adamjshook/accumulo/demo/app/model/DataFetcher.java
+++ accumulo-app/src/main/java/com/adamjshook/accumulo/demo/app/model/DataFetcher.java
@@ -4,14 +4,36 @@ import java.util.List;
 
 public abstract class DataFetcher
 {
+	/**
+	 * Gets the default data fetcher
+	 * 
+	 * @return
+	 */
 	public static DataFetcher getDefault()
 	{
 		return new GeneratedDataFetcher();
 	}
 
+	/**
+	 * Gets the tweets that contain the given word
+	 *
+	 * @param word
+	 *            Word to search for
+	 * @return A list of tweets containing the word, preferably a short one
+	 */
 	public abstract List<TweetBean> fetchTweetsFromIndex(String word);
 
+	/**
+	 * Gets the top ten trending hashtags
+	 *
+	 * @return
+	 */
 	public abstract List<String> fetchTrendingHashtags();
 
+	/**
+	 * Gets the top ten popular users
+	 *
+	 * @return
+	 */
 	public abstract List<String> fetchPopularUsers();
 }
